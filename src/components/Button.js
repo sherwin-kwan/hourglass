113f68bf900b5bbe2cfe062ab43719fa247cd55c
Sherwin <sherwin.kwan92@gmail.com>
2020-10-26 17:53:22 -0700

    add day list item file

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 import "components/Button.scss";
+import classnames from "classnames";
 
 export default function Button(props) {
-   return (<>
-    <button>{props.children}</button>
-   </>);
+  const theClass = classnames('button', {'button--confirm': props.confirm}, {'button--danger': props.danger});
+  const disabledStatus = (props.disabled) ? 'disabled' : null;
+  console.log('disabled status is', disabledStatus);
+  return (
+    <button className={theClass} disabled={props.disabled} onClick={props.onClick}>{props.children}</button>
+   );
 };
