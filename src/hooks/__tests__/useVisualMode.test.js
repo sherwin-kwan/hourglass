44269ca14af5aed14a3998aca15449ca144f2bc6
Sherwin <sherwin.kwan92@gmail.com>
2020-10-28 18:36:11 -0700

    add a history

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,5 +1,6 @@
 const FIRST = "FIRST";
 const SECOND = "SECOND";
+const THIRD = "THIRD";
 import { renderHook, act } from '@testing-library/react-hooks'
 import useVisualMode from '../useVisualMode';
 
@@ -14,4 +15,47 @@ test("useVisualMode should transition to another mode", () => {
 
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
+});
+
+
+
+
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+
+
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  // Passing "true" to transition(THIRD, true) says "Transition to THIRD by REPLACING SECOND"
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 });
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,13 +3,33 @@
 import React, { useState } from 'react';
 
 const useVisualMode = (initial) => {
-  const [mode, setMode] = useState(initial);
+  const [mode, setMode] = useState(initial);  
+  const [history, setHistory] = useState([initial]); // This line is new!
 
   const transition = (newMode) => {
-    setMode(newMode);
+    // setMode(newMode);
+    setHistory(prev => {
+      console.log('prev is: ', prev);
+      const arr = [...prev];
+      arr.push(newMode);
+      return arr;
+    })
   };
-  
-  return { mode, transition };
+
+  const back = () => {
+    console.log('in the beginning, history is: ', history)
+    if (history.length > 1) {
+      setHistory(prev => {
+        const arr = [...prev];
+        arr.pop();
+        return arr;
+      });
+      console.log('after popping, history is: ', history)
+      // setMode(history[history.length - 1]);
+    }
+  };
+
+  return { mode: history[history.length - 1], transition, back };
 };
 
 export default useVisualMode;
