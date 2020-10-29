8648b29fef6c7c8cf3002e1c136460008f3c1947
Sherwin <sherwin.kwan92@gmail.com>
2020-10-28 17:59:45 -0700

    start building useVisualMode hook

---
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -0,0 +1,17 @@
+const FIRST = "FIRST";
+const SECOND = "SECOND";
+import { renderHook, act } from '@testing-library/react-hooks'
+import useVisualMode from '../useVisualMode';
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,15 @@
+// Custom hook called useVisualMode
+
+import React, { useState } from 'react';
+
+const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+
+  const transition = (newMode) => {
+    setMode(newMode);
+  };
+  
+  return { mode, transition };
+};
+
+export default useVisualMode;
