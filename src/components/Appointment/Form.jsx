f1351b959f402da53b06dfa974d046e89231ba20
Sherwin <sherwin.kwan92@gmail.com>
2020-11-02 20:41:18 -0800

    added more form tests

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -27,10 +27,11 @@ const AppointmentForm = (props) => {
     if (name === "") {
       setError("Student name cannot be blank");
       return;
-    }
-
-    props.onSave(name, interviewer);
-  }
+    } else {
+      setError('');
+      props.onSave(name, interviewer);
+    };
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -32,7 +32,7 @@ describe('Appointment form', () => {
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
-  xit("validates that the student name is not blank", () => {
+  it("validates that the student name is not blank, but validation disappears once name is entered", () => {
 
     /* 1. Create the mock onSave function */
     const onSave = jest.fn(() => {
@@ -40,7 +40,7 @@ describe('Appointment form', () => {
     });
 
     /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
-    const { getByText } = render(<AppointmentForm
+    const { getByText, getByPlaceholderText, queryByText } = render(<AppointmentForm
       onSave={onSave}
       interviewers={interviewers}
       name={''}
@@ -54,44 +54,67 @@ describe('Appointment form', () => {
 
     /* 2. onSave is not called */
     expect(onSave).not.toHaveBeenCalled();
-  });
 
-  xit("calls onSave function when the name is defined", () => {
-
-    const onSave = jest.fn((name, interviewer) => {
-      console.log(`Saved, ${name} booked with interviewer no. ${interviewer}`);
+    // Next, test if it is possible to enter a name, save, and cancel the message
+    fireEvent.change(getByPlaceholderText("Enter your name"), {
+      target: { value: "Lydia Miller-Jones" }
     });
 
-    const { queryByText } = render(<AppointmentForm
-      name='Lydia Miller-Jones'
-      interviewers={interviewers}
-      onSave={onSave}
-    />);
-    fireEvent.click(queryByText("Save"));
-
-    /* 3. validation is not shown */
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-
-    /* 4. onSave is called once*/
-    expect(onSave).toHaveBeenCalledTimes(1);
-
-    /* 5. onSave is called with the correct arguments */
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 0);
-  });
-
-  it("submits the name entered by the user", () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <AppointmentForm interviewers={interviewers} onSave={onSave} />
-    );
-
-    const input = getByPlaceholderText("Enter your name");
-
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
     fireEvent.click(getByText("Save"));
 
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 0);
   });
 
-})
+  it("calls onCancel and resets the input field", () => {
+    const onCancel = jest.fn();
+    const { getByText, getByPlaceholderText, queryByText } = render(
+      <AppointmentForm
+        interviewers={interviewers}
+        name="Lydia Mill-Jones"
+        onSave={jest.fn()}
+        onCancel={onCancel}
+      />
+    );
+  
+    fireEvent.click(getByText("Save"));
+  
+    fireEvent.change(getByPlaceholderText("Enter your name"), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+  
+    fireEvent.click(getByText("Cancel"));
+  
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+  
+    expect(getByPlaceholderText("Enter your name")).toHaveValue("");
+  
+    expect(onCancel).toHaveBeenCalledTimes(1);
+  });
+  
+
+  // it("calls onSave function when the name is defined", () => {
+
+  //   const onSave = jest.fn((name, interviewer) => {
+  //     console.log(`Saved, ${name} booked with interviewer no. ${interviewer}`);
+  //   });
+
+  //   const { queryByText } = render(<AppointmentForm
+  //     name='Lydia Miller-Jones'
+  //     interviewers={interviewers}
+  //     onSave={onSave}
+  //   />);
+  //   fireEvent.click(queryByText("Save"));
+
+  //   /* 3. validation is not shown */
+  //   expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+  //   /* 4. onSave is called once*/
+  //   expect(onSave).toHaveBeenCalledTimes(1);
+
+  //   /* 5. onSave is called with the correct arguments */
+  //   expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 0);
+  // });
+});
